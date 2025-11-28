29b5c27799d81fce43dfa3b6947d7920b1be6936
Sam Baraka <barakaarunga@gmail.com>
2025-11-28 18:51:43 +0300

    added json file to hold about page decontent + added landing folder to hold about components

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,21 @@
+import { useEffect, useState } from "react"
+
+
+
 export default function About() {
-  return <h1>About Page</h1>
+
+  const [content, setContent] = useState(null)
+
+useEffect(()=>{
+  const data = fetch("/landing-content.json")
+  .then(res => res.json())
+  .then(data => setContent(data))
+  .catch(err => console.error(err))
+}, [])
+
+
+  if (!content) return <p>Loading...</p>;
+
+
+  return (<h1>{content.hero.headline}</h1>)
 }
